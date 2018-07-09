3b45c34b9c2f9015675be36c5ddab4a222d2a651
Jaco Erasmus <jierasmus27@gmail.com>
2018-07-09 23:08:16 +0200

    Add follower component and service

---
+++ src/app/follower/follower.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'follower',
+  templateUrl: './follower.component.html',
+  styleUrls: ['./follower.component.css']
+})
+export class FollowerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
